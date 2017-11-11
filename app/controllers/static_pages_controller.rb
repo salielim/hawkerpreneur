00535982143d4cf5a153fddf18fbaa567fa31cf4
Salie Lim <salie.lim@gmail.com>
2017-11-11 16:20:04 +0800

    add new routes, partial footer & header, add info page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,7 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+
+  def info
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/contact'
-
-  root 'static_pages#home'  
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get  '/info', to: 'static_pages#info'
+  get  '/contact', to: 'static_pages#contact'
+  # get  '/community', to: 'static_pages#community'
 end
