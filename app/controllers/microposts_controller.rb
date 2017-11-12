98e026bbda4675447fbccdcdd5827b25c5b0f79f
Salie Lim <salie.lim@gmail.com>
2017-11-12 11:55:59 +0800

    add form validations & error messages

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,9 +8,8 @@ class MicropostsController < ApplicationController
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = 'Micropost created!'
-      redirect_to root_url
     else
-      render 'static_pages/home'
+      flash[:error] = 'Micropost not created!'
     end
   end
 
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,5 +2,6 @@ class Micropost < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }  
   validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 2000 }  
+  validates :title, presence: true, length: { maximum: 60 }
+  validates :content, presence: true, length: { maximum: 2000 }
 end
