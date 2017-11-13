1630a1c8680f6bf19ebb88dc0c3f6824485008f9
Salie Lim <salie.lim@gmail.com>
2017-11-13 11:20:13 +0800

    show user image only if it exists, modify migration

--- db/migrate/20171112123329_fix_column_name.rb
+++ db/migrate/20171112123329_fix_column_name.rb
@@ -1,5 +1,5 @@
 class FixColumnName < ActiveRecord::Migration[5.0]
   def change
-    rename_column :microposts, :store_id, :hawker_centre
+    add_column :microposts, :hawker_centre, :string
   end
 end
